26a5915cec7c4d24c60fdd3346236a4a05170ca2
Nishanthini003 <nishanthiniram232gmail.com>
2024-02-08 21:39:59 +0530

    Your commit message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
-
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NoopAnimationsModule,  
+    AppRoutingModule,  
     HttpClientModule
-      
-    
   ],
   providers: [],
   bootstrap: [AppComponent]
